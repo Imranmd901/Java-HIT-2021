09404b35bf898e9a36d6be1a7fb85be5030fd4d2
Imranmd901 <md55imranmd@gmail.com>
2021-06-07 09:16:33 +0530

    initial commit

---
+++ AddTwoNumbers/src/AddTwoNumbers.java
@@ -0,0 +1,12 @@
+
+public class AddTwoNumbers {
+
+	public static void main(String[] args) {
+		int number1=200,number2=50,sum;
+		sum=number1+number2;
+		System.out.println("Sum of two numbers: "+sum);
+		
+
+	}
+
+}
